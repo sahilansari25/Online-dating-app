94fad2c8ae21850f8925fa1cedd9edc7fc37beb0
sahilansari25 <25sahilansari2001@gmail.com>
2021-12-11 19:20:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+
+const app = express();
+// environment var for port
+const port = process.envPORT || 3000;
+// setup view engine
+app.engine('handlebars',engine({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
+app.get('/',(req,res) => {
+    res.render('home',{
+        title: 'Home'
+    });
+});
+app.get('/about',(req,res) => {
+    res.render('about',{
+        title:'About'
+    });
+});
+app.get('/contacts',(req,res) => {
+    res.render('contacts',{
+        title: 'Contacts'
+    });
+});
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`);
+});
