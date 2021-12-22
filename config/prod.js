77b8556eefd6829cd298dcb3537f0ad014c9325a
sahilansari25 <25sahilansari2001@gmail.com>
2021-12-22 18:09:47 +0530

    push changes to heroku

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://onlineDatingApp:ansari2001@onlinedatingapp.ew6if.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    // FacebookAppID: '1215329305661606',
+    // FacebookAppSecret: '0bd546ac1afe8f1b73daaf756e5741f2'
+    GoogleClientID: '926211031579-7d7iodufsfrv3d26g2oifa785nrihr1v.apps.googleusercontent.com',
+    GoogleClientSecret: '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'
+};
